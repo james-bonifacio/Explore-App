670b024a5de5cba4a30730d09e9153ab71908d30
James <jameslezadabonifacio@gmail.com>
2018-02-08 04:36:37 -0500

    Added button and changed layout

--- app/src/main/java/com/example/james/exploreapp/MainActivity.java
+++ app/src/main/java/com/example/james/exploreapp/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.james.exploreapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnNext;
+    private String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnNext = findViewById(R.id.button_next);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+
+
+
+            }
+        });
     }
+
+
+
+
+
+
 }
