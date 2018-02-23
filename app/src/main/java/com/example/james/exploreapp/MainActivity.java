27d377f41c8d9f0e80285eeddbf6b9a26f40ce39
James <jameslezadabonifacio@gmail.com>
2018-02-22 22:53:54 -0500

    minor changes

--- app/src/main/java/com/example/james/exploreapp/MainActivity.java
+++ app/src/main/java/com/example/james/exploreapp/MainActivity.java
@@ -77,6 +77,9 @@ public class MainActivity extends AppCompatActivity {
                 // TODO: Handle the error.
                 Log.i(TAG, "An error occurred: " + status);
             }
+
+
+
         });
     }
 
