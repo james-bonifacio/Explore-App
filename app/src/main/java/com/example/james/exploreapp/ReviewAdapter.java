a4fb60a98854415860597a6ced3da24fec2d90ee
James <jameslezadabonifacio@gmail.com>
2018-03-05 22:55:28 -0500

    Added base review adapter class

---
+++ app/src/main/java/com/example/james/exploreapp/ReviewAdapter.java
@@ -0,0 +1,24 @@
+package com.example.james.exploreapp;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+/**
+ * Created by James on 2018-03-05.
+ */
+
+public class ReviewAdapter {
+
+
+}
