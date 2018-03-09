6ce6778c198e03721f9d8f18d878a73a6ec36522
James <jameslezadabonifacio@gmail.com>
2018-03-08 23:40:23 -0500

    Implemented ReviewAdapter

--- app/src/main/java/com/example/james/exploreapp/ReviewAdapter.java
+++ app/src/main/java/com/example/james/exploreapp/ReviewAdapter.java
@@ -8,9 +8,11 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.ms.square.android.expandabletextview.ExpandableTextView;
 
 import java.util.ArrayList;
 
@@ -18,7 +20,61 @@ import java.util.ArrayList;
  * Created by James on 2018-03-05.
  */
 
-public class ReviewAdapter {
+public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewHolder> {
+
+    private Context mCtx;
+    private ArrayList<Review> reviews;
+
+    public ReviewAdapter(Context mCtx, ArrayList<Review> reviews) {
+        this.mCtx = mCtx;
+        this.reviews = reviews;
+    }
 
 
+    @Override
+    public ReviewAdapter.ReviewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.review_layout, parent, false);
+        ReviewHolder holder = new ReviewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ReviewAdapter.ReviewHolder holder, int position) {
+
+        Review review = reviews.get(position);
+
+        holder.tvName.setText(review.getName());
+        holder.tvTime.setText(review.getTime());
+        holder.rating_rating_bar.setRating(review.getRating());
+        holder.expandable_text_view.setText(review.getText());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return reviews.size();
+    }
+
+
+    class ReviewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvName, tvTime;
+        RatingBar rating_rating_bar;
+        ExpandableTextView expandable_text_view;
+
+
+        public ReviewHolder(View itemView) {
+            super(itemView);
+
+            tvName = itemView.findViewById(R.id.tvName);
+            tvTime = itemView.findViewById(R.id.tvTime);
+            rating_rating_bar = itemView.findViewById(R.id.rating_rating_bar);
+            expandable_text_view = itemView.findViewById(R.id.expandable_text_view);
+
+        }
+    }
 }
