f8cbfce13501b4f979992582102b0bdd453766bf
James <jameslezadabonifacio@gmail.com>
2018-03-20 23:54:15 -0400

    Added route settings activity

---
+++ app/src/main/java/com/example/james/exploreapp/RouteSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.example.james.exploreapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RouteSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_settings);
+    }
+}
