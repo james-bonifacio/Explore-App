c660210f8f27dcccf5d9181a463e3c96534a3d5c
James <jameslezadabonifacio@gmail.com>
2018-03-22 23:44:34 -0400

    Added extras to intent of route settings

--- app/src/main/java/com/example/james/exploreapp/RouteSettingsActivity.java
+++ app/src/main/java/com/example/james/exploreapp/RouteSettingsActivity.java
@@ -3,11 +3,19 @@ package com.example.james.exploreapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class RouteSettingsActivity extends AppCompatActivity {
+
+    ArrayList<Location> locations;
+    String defaultStartLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route_settings);
+
+        locations = this.getIntent().getParcelableArrayListExtra("Locations");
+        defaultStartLocation = (String) this.getIntent().getExtras().get("City");
     }
 }
